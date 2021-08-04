61c4572e2bbbec72ded87c27137d0aadc4e68c92
Lamonte Scott <lamonteheli45@gmail.com>
2021-08-04 17:41:28 -0500

    added game var and beginning user prompt

---
+++ tripleX.cpp
@@ -0,0 +1,27 @@
+//Title: Triple X
+//Author: Lamonte Scott
+//Purpose: Terminal base guessing game for 
+//************************************
+
+//Preprocessor Directives
+#include <iostream>
+
+
+
+int main() 
+{
+    //Local Variables
+    int a = 4, b = 7, c = 2;
+    int sum = a + b + c;
+    int product = a * b * c;
+
+    std::cout << "You are a scret agent breaking into a secure serve room..." << std::endl;
+    std::cout << "Enter the correct code to continue..." << std::endl;
+
+
+
+
+
+
+    return 0;
+}
