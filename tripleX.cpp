06ce53e8260309413e88ad429bd6a3bdd3700a78
Lamonte Scott <lamonteheli45@gmail.com>
2021-08-04 19:06:33 -0500

    completed TripleX game

--- tripleX.cpp
+++ tripleX.cpp
@@ -1,27 +1,85 @@
 //Title: Triple X
 //Author: Lamonte Scott
-//Purpose: Terminal base guessing game for 
+//Purpose: Terminal base guessing game.
 //************************************
 
 //Preprocessor Directives
 #include <iostream>
+#include <ctime>
 
-
+//Function Prototype
+bool PlayGame (int Level);
+void PlayerIntrdouction(int Difficulty);
 
 int main() 
 {
-    //Local Variables
-    int a = 4, b = 7, c = 2;
-    int sum = a + b + c;
-    int product = a * b * c;
+    srand(time(NULL));
+    
+    int LevelDifficulty = 1;
+    const int maxDifficulty = 5;
 
-    std::cout << "You are a scret agent breaking into a secure serve room..." << std::endl;
+    while (LevelDifficulty <= maxDifficulty) //Loop game until are level are covered
+    {
+        bool bLevelComplete = PlayGame(LevelDifficulty);
+        std::cin.clear(); //Reset the working state / Discards the errors
+        std::cin.ignore(); //Discards the buffer
+
+        if (bLevelComplete)
+            {
+                //Increased the level difficulty
+                ++LevelDifficulty;
+            }
+    }
+   
+   std::cout << "\n*** Great work agrent! You got all the files! Now get out of there! **\n ";
+    
+    return 0;
+}
+
+void PrintIntroduction(int Difficulty)
+{
+      //Print welcome message to the terminal
+    std::cout << "\n\nYou are CodeA scret agent breaking into a level " << Difficulty << " a secure serve room..." << std::endl;
     std::cout << "Enter the correct code to continue..." << std::endl;
 
+}
 
 
+bool PlayGame (int Level)
+{
+    PrintIntroduction(Level);
 
+     //Local Variables
+    const int CodeA = rand() % Level + Level, CodeB = rand() % Level + Level, CodeC = rand() % Level + Level;
+    const int CodeSum = CodeA + CodeB + CodeC;
+    const int CodeProduct = CodeA * CodeB * CodeC;
+    int GuessA, GuessB, GuessC;
+    int GuessSum = GuessA + GuessB + GuessC;
+    int GuessProduct = GuessA + GuessB + GuessC;
 
+    
+    //Print CodeSum and CodeProduct to the terminal
+    std::cout << std::endl;
+    std::cout << "+ There are 3 numbers in the code." << std::endl;
+    std::cout << "+ The codes add-up to: " << CodeSum <<  std::endl;
+    std::cout << "+ The codes multiply to give: " << CodeProduct << std::endl;
 
-    return 0;
+    std::cout << "\nPlease enter your three guesses: ";
+    std::cin >> GuessA >> GuessB >> GuessC;
+    std::cout << "You entered: " << GuessA << " " << GuessB << " " << GuessC << std::endl;
+
+    //Check if player guess is correct
+    if (GuessSum == CodeSum && GuessProduct == CodeProduct)
+        {
+            std::cout << "\nCongrats, you broke the codes." << std::endl;
+            std::cout << "You Win\n";
+            return true;
+        }
+    else 
+        {
+            std::cout << "\nSorry, the codes did match." << std::endl;
+            std::cout << "You lose\n";
+            return false;
+        }
+
 }
